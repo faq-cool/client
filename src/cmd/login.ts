971c6407eefec99bd6df81e1ef49523c21be835c
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-05-29 07:53:42 +0300

    update

--- src/cmd/login.ts
+++ src/cmd/login.ts
@@ -13,6 +13,8 @@ export async function login() {
 
         res.writeHead(200, { 'Content-Type': 'text/plain' })
         res.end('You are now signed in! Token was saved to .env.faq. You can close me now.')
+
+        console.log('Token was saved to .env.faq')
         server.close(() => { })
     })
 
