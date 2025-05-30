9962fe43fce824ca4b541b6f53c6ed87da4872ae
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-05-30 21:11:47 +0300

    update

--- src/faqc.ts
+++ src/faqc.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import { appendFileSync } from 'fs'
 import tabtab from 'tabtab'
 import { version } from '../package.json'
 
@@ -29,6 +30,12 @@ export const cmdLs = program
     .command('ls')
     .description('List My faqs')
 
+// AUTH
+export const cmdAuth = program
+    .command('auth')
+    .option('-o, --out', 'Output json', 'auth.json')
+    .description('Authenticate and authentication data to json')
+
 // OPEN
 export const cmdOpen = program
     .command('open')
@@ -36,11 +43,6 @@ export const cmdOpen = program
     .option('-a, --auth', 'Authentication json', 'auth.json')
     .option('-u, --url <string>', 'URL to open')
     .option('--headless', 'Run in headless mode', false)
-
-// AUTH
-export const cmdAuth = program
-    .command('auth')
-    .description('Authenticate and save')
 
 // IT
 export const cmdIt = program.command('it <path.yml>')
@@ -51,13 +53,60 @@ export const cmdIt = program.command('it <path.yml>')
     .option('-d, --dry', 'Run in dry run mode')
     .option('-i, --id <number>', 'Update existing faq id', Number)
 
-function complete() {
+function getFlags(cmd: Command) {
+    return cmd.options.map(opt => {
+        const match = opt.flags.match(/(-\w)?(?:, )?(--\w[\w-]*)?/)
+        return {
+            short: match?.[1],
+            long: match?.[2],
+            description: opt.description || ''
+        }
+    })
+}
+
+async function complete() {
+    const cmds = program.commands.map(cmd => ({
+        name: cmd.name(),
+        description: cmd.description()
+    }))
+
+    const names = cmds.map(cmd => cmd.name)
+
     const env = tabtab.parseEnv(process.env)
+
     if (!env.complete) return
 
+    // DEBUGGING
+    const now = new Date()
+    const time = now.toTimeString().split(' ')[0]
+    const log = (...args: any[]) => appendFileSync('.faqc.log', `${time} ${args.join(' ')}\n`)
+
+    log(JSON.stringify(env, null, 2))
+
+    const words = env.line.split(' ')
+
+    // IGNORE COMPLEX COMPLETION FOR NOW
+    log('POINT', env.point, env.line.length)
+    if (env.point < env.line.length - 1) return
+
     // COMPLETE COMMAND
-    tabtab.log(program.commands.map(cmd =>
-        `${cmd.name()}\t${cmd.description()}`))
+    log('WORDS', words, words.length)
+    log('NAMES', names, names.includes(env.prev))
+    if (env.words <= 2 && !names.includes(env.prev)) {
+        const comp = cmds.map(cmd => `${cmd.name}\t${cmd.description}`)
+        log('COMPLETE COMMANDS', comp)
+        return tabtab.log(comp)
+    }
+
+    const defined = (x: any) => x !== undefined && x !== null
+    // COMPLETE OPTIONS
+    const cmd = words[1]
+    if (cmd == 'auth') {
+        const flags = getFlags(cmdAuth)
+        if (env.prev === '-') return tabtab.log(flags.map(f => f.short).filter(defined) as string[])
+        // const opts = cmdAuth.options
+        // return tabtab.log(cmdAuth.options.map(opt => `${opt.flags}\t${opt.description}`))
+    }
 }
 
 complete()
